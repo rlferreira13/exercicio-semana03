3888468d5c133b0e4b6eccaaa46e1471aed0303a
Ricardo Luiz Ferreira <ferreirarl@mdck.local>
2023-02-10 15:34:59 -0300

    Exercicio 9 e 10 resolvidos

---
+++ exercicio10.js
@@ -0,0 +1,12 @@
+const numeros = [1,3,5,7,9,11,13,15];
+
+/* usando o For Of */
+for (let i of numeros) {
+    console.log(i);
+}
+
+/* usando o for Each */
+numeros.forEach(numero=>console.log (numero));
+
+
+
